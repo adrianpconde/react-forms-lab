ee5a50a523fb4514a83bdc7825810d21ceb45aa3
Carlos del Prado <carlos.delpradomota@telefonica.com>
2022-09-09 21:26:14 +0100

    feat: add events components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 function App() {
   return (
-    <>
-    </>
+    <div className="container py-5">
+      
+    </div>
   );
 }
 
---
+++ src/components/events/event-item/EventItem.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function EventItem() {
+  return (
+    <div>EventItem</div>
+  )
+}
+
+export default EventItem;
